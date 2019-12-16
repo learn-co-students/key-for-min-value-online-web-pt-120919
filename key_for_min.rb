a76107fc221f1a8849e1f3d6de7911f14b84b05f
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-16 21:15:25 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,13 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+  name_hash = {"chair"=>4 ,"table"=>5}
+  name_hash.collect do |chair , table|
+  if chair < table 
+    puts chair
+    elsif chair > table 
+    puts table 
+  else 
+    false 
 
 end
+end 
+end 
