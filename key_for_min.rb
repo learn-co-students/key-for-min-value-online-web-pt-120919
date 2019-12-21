cc08ca8317911e1e7711e8506cea50d4ddab272a
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-21 17:58:00 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,2 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
-
-end
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
